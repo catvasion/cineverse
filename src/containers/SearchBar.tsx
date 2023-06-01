cad0071ccabc50904ceda603ffbeb67e64e9dd79
Emily Dawson <lemilydawson@gmail.com>
2023-06-01 15:07:39 +0100

    [feature_06_add-filter-options-to-SearchBar-component] SearchBar component added responsive filter options drop down menu in input

--- src/containers/SearchBar.tsx
+++ src/containers/SearchBar.tsx
@@ -1,19 +1,20 @@
 import React, { useState, ChangeEvent, KeyboardEvent } from "react";
-import { Box, Input, IconButton, Spinner, HStack } from "@chakra-ui/react";
+import { Box, Input, IconButton, Spinner, HStack, InputGroup, InputRightElement, Select } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { useBreakpointValue } from "@chakra-ui/react";
 
 interface SearchBarProps {
-  onSearch: (searchTerm: string) => void;
+  onSearch: (searchTerm: string, filterOption: string) => void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [filterOption, setFilterOption] = useState("")
   const [isLoading, setIsLoading] = useState(false);
 
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      onSearch(searchTerm);
+      onSearch(searchTerm, filterOption);
     }
   };
 
@@ -21,25 +22,48 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     setSearchTerm(event.target.value);
   };
 
+  const handleFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setFilterOption(event.target.value)
+  }
+
   const inputSize = useBreakpointValue({ base: "sm", md: "md", lg: "lg" });
+  
+  const selectPlaceholder = inputSize === 'sm' ?  '' : "Filter Options"
 
   return (
     <HStack spacing={4} mb={4}>
-      <Input
-        placeholder="Search for a Movie"
-        size={inputSize}
-        value={searchTerm}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-      />
-      <IconButton
-        aria-label="Search database"
-        icon={isLoading ? <Spinner /> : <SearchIcon />}
-        onClick={() => {
-          onSearch(searchTerm);
-        }}
-      />
+      <InputGroup size={inputSize}>
+        <Input
+          placeholder="Search for a Movie"
+          size={inputSize}
+          value={searchTerm}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <InputRightElement width='auto'>
+         <Select
+            size={inputSize}
+            value={filterOption}
+            onChange={handleFilterChange}
+            placeholder={selectPlaceholder}
+            variant='unstyled' 
+          >
+          <option value=''></option>
+          <option value='year'>Year</option>
+          <option value='type'>Type</option>
+          </Select>   
+        </InputRightElement>
+        </InputGroup>
+        <IconButton
+          aria-label="Search database"
+          icon={isLoading ? <Spinner /> : <SearchIcon />}
+          onClick={() => {
+            onSearch(searchTerm, filterOption);
+          }}
+        />
+      
     </HStack>
+   
   );
 };
 
