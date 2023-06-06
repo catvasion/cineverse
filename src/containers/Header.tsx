f15de240483474e7ce079458c47d2930604890e3
Emily Dawson <lemilydawson@gmail.com>
2023-06-06 15:06:56 +0100

    [feature_07_add-ToggleLogo-component] ToggleLogo added for logo to change to appropriate colour when colorMode button is toggled

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,12 +1,19 @@
-import { Heading } from "@chakra-ui/react";
-import { PreferencesFlyout }from '../components'
+import { Heading, Image, Box } from "@chakra-ui/react";
+import { PreferencesFlyout, ToggleLogo }from '../components'
 import React from "react";
+
 
 const Header: React.FC = () => {
   return (
-    <Heading as="h1" mb={4} justifyContent='space-between' display='flex'>
-      MoviesRUs
-      <PreferencesFlyout />
+    <Heading as="h1" mb={4} justifyContent='space-evenly' display='flex'>
+      <Box m={9} display='flex' justifyContent='space-evenly'>
+      <Box>
+        <ToggleLogo src='getLogoSrc()'/>
+      </Box>
+      <Box ml={6}>
+        <PreferencesFlyout />
+      </Box>
+      </Box>
     </Heading>
   );
 };
