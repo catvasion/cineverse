5b43329c3f6d4db57b3626ec785dfb9b0f2b7874
Emily Dawson <lemilydawson@gmail.com>
2023-06-14 12:56:47 +0100

    [change_09_input-colour] input and focus border colour changes to match logo colour depending on colormode

---
+++ src/components/ToggleInputBorder.tsx
@@ -0,0 +1,34 @@
+import { useColorMode, Input } from '@chakra-ui/react';
+import React, {FC} from "react"
+
+interface ToggleLogoProps {
+    focusBorderColor:string 
+}
+
+
+
+const ToggleInputBorder: FC<ToggleLogoProps> = () => {
+    const { colorMode } = useColorMode()
+
+
+const DarkModeCocusBorderColor = "#7FD2FF"
+const LightModeCocusBorderColor = "#121B7D"
+
+const getBoderColor = () => {
+    if (colorMode === 'light') {
+        return (LightModeCocusBorderColor)
+    } else {
+        return (DarkModeCocusBorderColor)
+    }
+}
+return (
+    <>
+    
+     focusBorderColor={getBoderColor()}
+    
+    </>
+
+)
+}
+
+export default ToggleInputBorder
--- src/containers/SearchBar.tsx
+++ src/containers/SearchBar.tsx
@@ -1,16 +1,20 @@
 import React, { useState, ChangeEvent, KeyboardEvent } from "react";
-import { Box, Input, IconButton, Spinner, HStack, InputGroup, InputRightElement, Select } from "@chakra-ui/react";
+import { Box, Input, IconButton, Spinner, HStack, InputGroup, InputRightElement, Select, useColorMode } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { useBreakpointValue } from "@chakra-ui/react";
+// import { ToggleInputBorder } from "../components";
+import "./customInput.css"
 
 interface SearchBarProps {
   onSearch: (searchTerm: string, filterOption: string) => void;
 }
 
+
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filterOption, setFilterOption] = useState("")
   const [isLoading, setIsLoading] = useState(false);
+  const { colorMode } = useColorMode()
 
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
@@ -27,6 +31,19 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
   // }
 
   const inputSize = useBreakpointValue({ base: "sm", md: "md", lg: "lg" });
+
+
+
+const DarkModeColor = "#7FD2FF"
+const LightModeColor = "#121B7D"
+
+const getColor = () => {
+  if (colorMode === 'light') {
+      return (LightModeColor)
+  } else {
+      return DarkModeColor
+  }
+}
   
 
   return (
@@ -34,21 +51,19 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
       <InputGroup size={inputSize}>
         <Input
           placeholder="Search"
-          _placeholder={{ opacity: 0.7, color: 'inherit' }}
+          // _placeholder={{ opacity: 0.7, color: 'inherit' }}
           size={inputSize}
           value={searchTerm}
           onChange={handleChange}
           onKeyDown={handleKeyDown}
-          borderColor='pink'
-          bgGradient="linear(to-l, #7928CA, #FF0080)"
-          color='white'
           fontWeight="bold"
-      
-          
+          focusBorderColor={getColor()}
+          color={getColor()}
         />
         
         </InputGroup>
-        <IconButton        
+        <IconButton  
+              
           aria-label="Search database"
           icon={isLoading ? <Spinner /> : <SearchIcon />}
           onClick={() => {
@@ -60,5 +75,4 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
    
   );
 };
-
 export default SearchBar;
