1e3edfa010f5c75502876a5a69da07c1d7a905ae
Emily Dawson <lemilydawson@gmail.com>
2023-07-06 14:52:30 +0100

    [change_23_images-in-screens] changed to screen01 and screen02

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,7 @@
 // import { DarkMode } from "@chakra-ui/react"
-import logo from "./logo.png"
-import logo_dark from './logo_dark.png'
-import logo_light from './logo_light.png'
 
-export {
-    logo,
-    logo_dark,
-    logo_light,
-}
+import logo from "./logo.png";
+import logo_dark from "./logo_dark.png";
+import logo_light from "./logo_light.png";
+
+export { logo, logo_dark, logo_light };
